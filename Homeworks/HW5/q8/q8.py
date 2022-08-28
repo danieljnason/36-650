96dd8967782e5f88b7173d893e717ed837c71c6d
danieljnason <89809251+danieljnason@users.noreply.github.com>
2022-08-28 18:42:48 -0400

    Update q8.py

--- Homeworks/HW5/q8/q8.py
+++ Homeworks/HW5/q8/q8.py
@@ -4,7 +4,7 @@ import psycopg2
 conn = psycopg2.connect(host="localhost", 
                         port="5432", 
                         user="postgres", 
-                        password="Daniel@895", 
+                        password="********", 
                         database="postgres"
                        )
 cur = conn.cursor()
